820e2ae1c27a178875951e9d5355dc4a5a5540db
Palkovmikus <palkovmike@gmail.com>
2019-11-27 22:05:19 +0300

    Java+Function+Arr+Stack

---
+++ 27.11.19/Function.js
@@ -0,0 +1,27 @@
+var Login;
+var Paasowd;
+
+function sal_hello() {
+  alert ("Hello");
+  console.log('hello');
+}
+        sal_hello();
+            function Message (){
+                login = prompt("Введите логин ");
+                Paasowd = prompt (" Введите пароль");
+                                }
+Message();
+
+function sum(a, b) {
+        return a + b;
+}
+
+switch (sing) {
+    case "+":
+        result = sum (oprator1 + operator2);
+        break;
+        
+                }
+                
+
+
--- 27.11.19/Java4.js
+++ 27.11.19/Java4.js
@@ -1,17 +1,42 @@
-//var myHeading = document.querySelector('h1');
-//myHeading.textContent = 'Hello world!';
-//alert("Hello");
-//debugger;
-//prompt("Input word for with idea");
-/*  var myImage = document.querySelector('img');
+function toStr(el=new Array()){
+    var ret='';
+    el.forEach(function(e) { //не совсем понял как сработало.
+        ret+=e;
+        //console.log(e);
+    });
+    return ret;
+}
+//
+var t=document.getElementById('target');
+var a = new Array('*');
+//var b = new Array();
+t.innerHTML+=a[0]+'<br>';
+while(a.length<33){
+    //b = b.concat(' ');
+    a = a.concat('*' , '*');
+    t.innerHTML+=(toStr(a)/*+toStr(b)*/+'<br>');
+}; // стилизировать по центру програмно не получилось,
+// только через text-align
 
-myImage.onclick = function()
- {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/Devil.jpg') {
-      myImage.setAttribute ('src','images/honda.jpg');
-    } else {
-      myImage.setAttribute ('src','images/bmv.jpg ');
-    }
-  }
-   */
+// Попытка 2 не удалась, ошибка в коде
+// не смог понять где, а вроде всё правильно
+
+/*var t2=document.getElementById('target2');
+function printEl(c, h=31){
+    var tree=new Array();
+    tree.fill(' ', h-1/c%2, c);
+    tree.fill('*', c);
+    console.log(tree);
+    document.write(toStr(tree)+'<br>');
+}
+var c = 1;
+do{
+    printEl(c);
+    c++
+}while(c<11);*/
+for (let i = 1; i <=32; i+=2){ //  делаем цикл основание в 30 ел
+    for (let n = 1; n < i; n+=2) 
+    //  делаем вложенный цикл который с каждой итерацией уменьшает i на 2
+    document.write('*' + n); 
+    document.write('</br>');
+}
