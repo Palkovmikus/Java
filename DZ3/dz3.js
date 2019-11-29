d0372dc82c31f0fe3095482da49b44984c89f9b1
Palkovmikus <palkovmike@gmail.com>
2019-11-29 15:21:46 +0200

    DZ1,2,3,4

---
+++ DZ3/dz3.js
@@ -0,0 +1,17 @@
+//var myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello world!';
+//alert("Hello");
+//debugger;
+//prompt("Input word for with idea");
+/*  var myImage = document.querySelector('img');
+
+myImage.onclick = function()
+ {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/Devil.jpg') {
+      myImage.setAttribute ('src','images/honda.jpg');
+    } else {
+      myImage.setAttribute ('src','images/bmv.jpg ');
+    }
+  }
+   */
